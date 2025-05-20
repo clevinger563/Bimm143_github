77af42644525ced39b3a23fdeeaf2d3ba142d087
Christopher Levinger <clevinger@ucsd.edu>
2025-05-20 12:36:50 -0700

    Add Class 4

---
+++ class04/class04.R
@@ -0,0 +1,9 @@
+#' ---
+#' title: "Intro to R"
+#' author: "Christopher Levinger"
+#' date: "April 10, 2025"
+#' ---
+# first R script
+x<-1:50
+plot(x,sin(x))
+plot(x,sin(x), type="l",col="red", lwd=3, xlab="Silly x axis", ylab="Sensible y axis")
